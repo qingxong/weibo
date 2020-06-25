d8e344651d2ce583c48f8f16109c0faf023e4cda
qingxong <465016447@qq.com>
2020-06-25 05:30:49 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@
 route::get('/','StaticPagesController@home')->name('home');
 route::get('/help','StaticPagesController@help')->name('help');
 route::get('/about','StaticPagesController@about')->name('about');
+route::get('/signup',"UsersController@create")->name('signup');
